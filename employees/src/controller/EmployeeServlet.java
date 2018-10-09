a966cfe8605b6e11393766c14efeb9d486dbacc3
Sigh <yxh644166727@gmail.com>
2018-10-09 16:14:14 +0700

    update

--- employees/src/controller/EmployeeServlet.java
+++ employees/src/controller/EmployeeServlet.java
@@ -9,6 +9,7 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -29,7 +30,7 @@ public class EmployeeServlet extends HttpServlet {
         String birth_date = request.getParameter("birth_date");
         String first_name = request.getParameter("first_name");
         String last_name = request.getParameter("last_name");
-        String gender_id = request.getParameter("gender_id");
+        String gender = request.getParameter("gender");
         String hire_date = request.getParameter("hire_date");
 
 
@@ -42,7 +43,7 @@ public class EmployeeServlet extends HttpServlet {
         out.println(birth_date+"<br/>");
         out.println(first_name+"<br/>");
         out.println(last_name+"<br/>");
-        out.println(gender_id+"<br/>");
+        out.println(gender+"<br/>");
         out.println(hire_date+"<br/>");
 
         Context envContext = null;
@@ -57,7 +58,7 @@ public class EmployeeServlet extends HttpServlet {
 //            Context initContext  = (Context)envContext.lookup("java:/comp/env");
             Context initContext = envContext;
 //            DataSource ds = (DataSource)initContext.lookup("jdbc/affableBean");
-            DataSource ds = (DataSource)envContext.lookup("java:/comp/env/jdbc/employees_partitioned_5.1");
+            DataSource ds = (DataSource)envContext.lookup("java:/comp/env/jdbc/employees");
             Connection con = ds.getConnection();
 
 
@@ -70,24 +71,25 @@ public class EmployeeServlet extends HttpServlet {
                 stmt.execute();
             } else if (cmd != null && cmd.equals("u")) {
                 // Update a product
-                sql = "UPDATE employees SET birth_date = ?, first_name = ?, last_name = ?, gender_id = ?, hire_date = ? WHERE emp_no = ?";
+                sql = "UPDATE employees SET birth_date = ?, first_name = ?, last_name = ?, gender = ?, hire_date = ? WHERE emp_no = ?";
                 PreparedStatement stmt=con.prepareStatement(sql);
-                stmt.setString(1,birth_date);
+                stmt.setDate(1, Date.valueOf(birth_date));
                 stmt.setString(2,first_name);
                 stmt.setString(3,last_name);
-                stmt.setInt(4,Integer.parseInt(gender_id));
-                stmt.setInt(5,Integer.parseInt(emp_no));
+                stmt.setString(4,gender);
+                stmt.setDate(5, Date.valueOf(hire_date));
                 stmt.execute();
 
             } else {
                 // Insert a new product
-                sql = "insert into product (birth_date, first name, last_name, gender_id, hire_date)  values (?,?,?,?,?)";
+                sql = "insert into employees (emp_no, birth_date, first_name, last_name, gender, hire_date)  values (?,?,?,?,?,?)";
                 PreparedStatement stmt=con.prepareStatement(sql);
-                stmt.setString(1,birth_date);
-                stmt.setString(2,first_name);
-                stmt.setString(3,last_name);
-                stmt.setInt(4,Integer.parseInt(gender_id));
-                stmt.setString(5,hire_date);
+                stmt.setInt(1, Integer.parseInt(emp_no));
+                stmt.setDate(2, Date.valueOf(birth_date));
+                stmt.setString(3,first_name);
+                stmt.setString(4,last_name);
+                stmt.setString(5,gender);
+                stmt.setDate(6, Date.valueOf(hire_date));
                 stmt.execute();
             }
 //            out.println("Row affect "+r+"<br>");
